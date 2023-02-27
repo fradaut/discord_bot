c515aa223b50684c7a08c455aa40093bcf477188
NFalone <qoo19830526@gmail.com>
2023-02-27 23:51:06 +0800

    feat: 變數client存入pinia 將變數client存入pinia內

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import vueInit from '@/core/vue';
 import dotenv from 'dotenv';
 
 import { loadCommands, loadEvents } from '@/core/loader';
+import { useAppStore } from '@/store/app';
 
 vueInit();
 dotenv.config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
+const appStore = useAppStore()
+appStore.client = client;
 
 loadCommands();
 loadEvents();
