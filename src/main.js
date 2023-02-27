984edb08f2f88ed69ee121b24f1d7e43f31b96cc
NFalone <qoo19830526@gmail.com>
2023-02-27 20:58:59 +0800

    feat: discord機器人 啟動機器人基本配置

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import {Client, Events, GatewayIntentBits} from 'discord.js';
+import vueInit from '@/core/vue';
+import dotenv from 'dotenv';
+
+vueInit();
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
