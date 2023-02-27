868921cec17a7c5566f1b2bd41a2a6ab55d1681d
NFalone <qoo19830526@gmail.com>
2023-02-28 02:33:07 +0800

    refactor: 更改ping指令描述 更改指令描述

--- src/commands/ping/index.js
+++ src/commands/ping/index.js
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from "discord.js";
 
 export const command = new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("ping command");
+    .setDescription("測試指令");
 
 export const action = async (interaction) => {
     interaction.reply("pong!");
